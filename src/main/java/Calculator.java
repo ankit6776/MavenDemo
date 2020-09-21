4147a2277e7cd4ad11bb0fa906f5af237a958ded
Ankit Chaudhary <ankit.chaudhary@phonepe.com>
2020-09-21 14:24:32 +0530

    just to check library

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(Calculation.sub(2,3));
+        System.out.println(Calculation.add(2,3));
+        System.out.println(Calculation.mul(2,3));
+    }
+}
